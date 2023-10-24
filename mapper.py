1855dbd39e61b761316cb7572c58f566969bcd8b
IHaidov <38441739+IHaidov@users.noreply.github.com>
2023-10-24 17:29:10 +0200

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+
+import sys
+import csv
+
+reader = csv.reader(sys.stdin)
+
+for values in reader:
+    if len(values) < 16:
+        continue  
+
+    developer_id = values[-1]
+    rating = values[3]  
+    rating_count = values[4].replace(",", "")  
+    released_date = values[14]
+
+    if not rating or not rating_count or not released_date:
+        continue
+
+    try:
+        rating = float(rating)
+        rating_count = int(rating_count)
+        year = released_date.split(" ")[0].split("-")[0]
+    except ValueError:
+        continue
+
+    if rating_count >= 1000:
+        print("%s,%s\t%f\t%d\t1" % (developer_id, year, rating, rating_count))
---
+++ reducer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+
+import sys
+
+current_key = None
+total_rating = 0.0
+total_rating_count = 0
+app_count = 0
+
+print("dev_id year total_rating total_rating_count app_count")
+for line in sys.stdin:
+    line = line.strip()
+    developer_year, rating, rating_count, app = line.split("\t")
+
+    rating = float(rating)
+    rating_count = int(rating_count)
+    app = int(app)
+
+    if current_key == developer_year:
+        total_rating += rating
+        total_rating_count += rating_count
+        app_count += app
+    else:
+        if current_key:
+            dev_id, year = current_key.split(",")
+            print("%s\t%s\t%f\t%d\t%d" % (dev_id, year, total_rating, total_rating_count, app_count))
+
+        total_rating = rating
+        total_rating_count = rating_count
+        app_count = app
+        current_key = developer_year
+
+if current_key == developer_year:
+    dev_id, year = current_key.split(",")
+    print("%s\t%s\t%f\t%d\t%d" % (dev_id, year, total_rating, total_rating_count, app_count))
